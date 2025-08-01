49401bf630cd9bddb161c299f5a071834536fb7a
anonimo605 <anonimo.tututu@gmail.com>
2025-07-31 19:47:33 -0500

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCGL6jCEV8SIhIfuS-O6ylG14gh5WYfCYM",
-  authDomain: "satelital-9416f.firebaseapp.com",
-  projectId: "satelital-9416f",
-  storageBucket: "satelital-9416f.appspot.com",
-  messagingSenderId: "803736254270",
-  appId: "1:803736254270:web:59f1d326ef4e592061de6f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!
 };
 
 // Initialize Firebase
@@ -19,5 +18,3 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
-    
